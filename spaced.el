29e73b34207cb4f3d83dbdd7e791335aca9473fc
justin talbott <justin@waymondo.com>
2025-04-20 10:29:13 -0400

    add basic header

--- spaced.el
+++ spaced.el
@@ -1,4 +1,29 @@
-;;; spaced.el --- -*- lexical-binding: t -*-
+;;; spaced.el --- Automatically pad your code with spaces -*- lexical-binding: t -*-
+
+;; Copyright (C) 2025 Justin Talbott
+
+;; Author: Justin Talbott
+;; URL: https://github.com/waymondo/spaced
+;; Package-Requires: ((emacs "24"))
+;; License: GNU General Public License version 3, or (at your option) any later version
+;; Keywords: convenience, tools
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;;; Code:
 
 (setq spaced-vectors-alist
       '(
